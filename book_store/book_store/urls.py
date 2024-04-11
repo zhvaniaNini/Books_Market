3554ef3a249bb9dfd9623ae47cb96d8202515e58
Nini <160964486+zhvaniaNini@users.noreply.github.com>
2024-04-11 17:02:39 +0400

    Update urls.py

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -22,5 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listed_books, name='listed_books')
-]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
