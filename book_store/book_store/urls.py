6e29344387aa95cbd3fb8a975b79f34a36d51929
Nini <160964486+zhvaniaNini@users.noreply.github.com>
2024-04-11 17:00:43 +0400

    Update urls.py

--- book_store/book_store/urls.py
+++ book_store/book_store/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from market.views import listed_books
-
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', listed_books, name='listed_books')
-]
+]+ static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
 
